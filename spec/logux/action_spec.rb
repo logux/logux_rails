99b0c219610d1d3b7aca5b2aa8d0246b685b63b7
WildDima <dtopornin@gmail.com>
2018-06-13 13:23:26 +0300

    Update action spec

--- spec/logux/action_spec.rb
+++ spec/logux/action_spec.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
 
 describe Logux::Action do
   let(:action) { described_class.new(params: params, meta: meta) }
-  let(:params) { {} }
+  let(:params) { Logux::Params.new(type: 'logux/subscribe', channel: 'user/1') }
+  let(:user) { User.find_or_create_by(id: 1, name: 'test') }
   let(:meta) { {} }
 
   describe '#subscribe' do
     subject { action.subscribe }
 
-    context 'when ActiveRecord undefined' do
-      it 'raises error' do
-        expect { subject }.to raise_error(Logux::Action::UnknownTypeError)
+    context 'when ActiveRecord defined' do
+      it 'tries to find record by chanel data' do
+        expect(subject).to eq(user)
       end
     end
   end
