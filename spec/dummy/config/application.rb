64804adb56481d8da5f5def2c01bc9b0febd0fa6
WildDima <dtopornin@gmail.com>
2018-06-13 13:23:04 +0300

    Update dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,11 +3,13 @@
 require_relative 'boot'
 
 require 'rails'
+require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
 Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
