d1c08960b12311495292d9b3a78578e08a65ff82
WildDima <dtopornin@gmail.com>
2018-07-04 14:08:52 +0300

    Add class finder, add specs

---
+++ lib/logux/class_finder.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Logux
+  class ClassFinder
+    attr_reader :params
+
+    def initialize(params)
+      @params = params
+    end
+
+    def find_action_class
+      "Actions::#{class_name.camelize}".constantize
+    rescue NameError
+      raise Logux::NoActionError, %(
+        Unable to find action #{class_name.camelize}
+        Should be in app/logux/actions/#{class_path}.rb
+      )
+    end
+
+    def find_policy_class
+      "Policies::#{class_name.camelize}".constantize
+    rescue NameError
+      raise Logux::NoPolicyError, %(
+        Unable to find policy #{class_name.camelize}
+        Should be in app/logux/policies/#{class_path}.rb
+      )
+    end
+
+    def class_name
+      if params.type == 'logux/subscribe'
+        params.channel_name
+      else
+        params.type&.split('/')[0..-2]&.map(&:camelize)&.join('::')
+      end
+    end
+
+    def class_path
+    end
+  end
+end
---
+++ spec/logux/class_finder_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Logux::ClassFinder do
+  let(:finder) { described_class.new(params) }
+
+  describe '#class_name' do
+    subject { finder.class_name }
+
+    let(:params) { create(:logux_params_add, type: 'test/test/name/try/user/add') }
+
+    it 'returns nested classes' do
+      expect(subject).to eq('Test::Test::Name::Try::User')
+    end
+  end
+end
