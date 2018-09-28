c1ecaf0170b2e058e22ee63725bf4abf0afc6a04
wilddima <dtopornin@gmail.com>
2018-09-28 12:57:17 +0300

    Revert text, change time format

--- lib/logux/meta.rb
+++ lib/logux/meta.rb
@@ -6,7 +6,7 @@ module Logux
 
     def initialize(source_hash = nil, default = nil, &blk)
       super
-      self[:time] ||= Time.zone&.now&.to_i || Time.now.to_i
+      self[:time] ||= (Time.now.to_datetime.strftime('%Q'))
     end
 
     def to_s
