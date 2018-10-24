c5e526dddf7c29faa569d69ec329927560aefa01
Salahutdinov Dmitry <dsalahutdinov@gmail.com>
2018-10-24 10:41:30 +0500

    chore: elliminate factory_bot warnings

--- spec/factories/logux_actions_factory.rb
+++ spec/factories/logux_actions_factory.rb
@@ -3,20 +3,20 @@
 FactoryBot.define do
   factory :logux_actions, class: Logux::Actions do
     factory :logux_actions_subscribe do
-      type 'logux/subscribe'
-      channel 'user/1'
+      type { 'logux/subscribe' } 
+      channel { 'user/1' }
     end
 
     factory :logux_actions_add do
-      type 'user/add'
-      key 'name'
-      value 'test'
+      type { 'user/add' }
+      key { 'name' }
+      value { 'test' }
     end
 
     factory :logux_actions_update do
-      type 'user/update'
-      key 'name'
-      value 'test1'
+      type { 'user/update' }
+      key { 'name' }
+      value { 'test1' }
     end
   end
 end
