815f483e50b06766403e1c2ce9ef453e84715fd7
Alex Musayev <alex.musayev@gmail.com>
2019-10-03 20:05:21 +0300

    Merge root files to logux_rails.rb
    It makes sense to move root module to lib/logux/rails.rb after the gem is renamed to logux-rails. Gems naming convention: https://guides.rubygems.org/name-your-gem/

--- lib/logux_rails.rb
+++ lib/logux_rails.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
-require 'logux'
+require 'action_controller'
+require 'active_support'
+require 'logux/rack'
+require 'rails/engine'
+require 'logux/engine'
+
+module Logux
+  autoload :Model, 'logux/model'
+
+  configuration_defaults do |config|
+    config.logger = Rails.logger if defined?(Rails.logger)
+    config.logger ||= ActiveSupport::Logger.new(STDOUT)
+  end
+end
