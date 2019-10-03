f42926f5c9904f8df73056d2465141dba00269fe
Alex Musayev <alex.musayev@gmail.com>
2019-10-03 19:44:36 +0300

    Remove Logux facilities migrated to logux-rack gem
    - Remove redundant definitions from the Logux module
    - Remove ActiveSupport::Autoload module from Logux, since all core modules were autoloaded with Kernel#autoload, and an explicit load path is used anyway

--- lib/logux/action_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Logux
-  class ActionController < Logux::BaseController
-  end
-end
--- spec/logux/action_caller_spec.rb
+++ spec/logux/action_caller_spec.rb
@@ -7,32 +7,6 @@ describe Logux::ActionCaller do
   let(:action) { create(:logux_actions_add) }
   let(:meta) { create(:logux_meta) }
 
-  describe '#call!' do
-    context 'when action defined' do
-      subject(:result) { action_caller.call! }
-
-      before do
-        module Actions
-          class User < Logux::ActionController
-            def add
-              respond(:ok)
-            end
-          end
-        end
-      end
-
-      after do
-        Actions::User.send :undef_method, :add
-        Actions.send :remove_const, :User
-        Actions.send :const_set, :User, Class.new
-      end
-
-      it 'return ok' do
-        expect(result.status).to eq(:ok)
-      end
-    end
-  end
-
   context 'when attributes updated' do
     context 'when insecure #update is called' do
       let(:action) { create(:logux_actions_post, type: 'post/rename') }
